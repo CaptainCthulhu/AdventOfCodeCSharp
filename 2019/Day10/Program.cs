75e943ef89870c1130dd3a12122fa1000c754b50
CaptainCthulhu <theonegod@gmail.com>
2019-12-11 18:23:01 -0700

    Had the grid upside down.

--- 2019/Day10/Program.cs
+++ 2019/Day10/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Numerics;
-using System.Windows;
 
 namespace Day10
 {
@@ -17,15 +15,15 @@ namespace Day10
 
         static List<Point> split(string s)
         {
-            List<Point> stars = new List<Point>();            
+            List<Point> stars = new List<Point>();
 
             var rows = s.Split("\r\n").ToList();
-            rows.ForEach(x => x.Trim());          
+            rows.ForEach(x => x.Trim());
 
             for (int y = 0; y < rows.Count; y++)
             {
                 for (int x = 0; x < rows[0].Length; x++)
-                {                    
+                {
                     if (rows[y][x] == '#')
                         stars.Add(new Point(x, y));
                 }
@@ -62,7 +60,7 @@ namespace Day10
                         vector = new Point(vector.X / gcd, vector.Y / gcd);
                     }
                     if (!(vectorList.Any(v => v.X == vector.X && v.Y == vector.Y)))
-                    {                       
+                    {
                         vectorList.Add(vector);
                     }
 
@@ -79,7 +77,7 @@ namespace Day10
         }
 
 
-        private  static int GCD(int a, int b)
+        private static int GCD(int a, int b)
         {
             a = Math.Abs(a);
             b = Math.Abs(b);
@@ -88,7 +86,7 @@ namespace Day10
                 return 0;
 
             // Pull out remainders.
-            while(true)
+            while (true)
             {
                 int remainder = a % b;
                 if (remainder == 0) return b;
@@ -97,18 +95,81 @@ namespace Day10
             };
         }
 
-        static void Q1()
-        {            
-            Vectors(split(Input.example));            
-            Vectors(split(Input.example2));            
-            Vectors(split(Input.example3));            
-            Vectors(split(Input.real));
+        private static void Eliminate(List<Point> stars, Point shooter)
+        {
+            stars.Remove(shooter);
 
-            Console.ReadKey();
+            List<TargetDetails> targetDetails = new List<TargetDetails>();
+            stars.ForEach(x => targetDetails.Add(TargetDetails.CreateTarget(shooter, x)));
+
+            var degrees = targetDetails.Select(x => x.Degrees);
+            var degress2 = degrees.Distinct();
+            var degrees3 = degress2.ToList();
+            degrees3.Sort();
+            Console.WriteLine($"Sanity check: {degrees3.Count} count.");
+
+            Dictionary<decimal, List<TargetDetails>> groupDetails = new Dictionary<decimal, List<TargetDetails>>();
+
+            foreach (var degree in degrees3)
+            {
+                List<TargetDetails> degreeList = targetDetails.Where(x => x.Degrees == degree).ToList();
+                degreeList.Sort((emp1, emp2) => emp1.Distance.CompareTo(emp2.Distance));
+                groupDetails[degree] = degreeList;
+            }
+
+            int eliminated = 0;
+            Point target = new Point();
+            int goal = 200;
+            List<TargetDetails> eliminatedTargets = new List<TargetDetails>();
+
+            bool start = true;
+
+            while (eliminated < goal)
+            {
+                foreach (var angle in degrees3)
+                {
+                    if (start && angle < 270)
+                    {
+                    }
+                    else
+                    {
+                        start = false;
+
+                        var targets = groupDetails[angle];
+                        eliminatedTargets.Add(targets[0]);
+                        target = targets[0].Location;
+                        Console.WriteLine($"Target #{eliminated + 1} eliminated at {target.X},{target.Y}, angle {targets[0].Degrees}, distance {targets[0].Distance}. Value: {target.X * 100 + target.Y}. Left at angle {targets[0].Degrees}? {targets.Count()}");
+                        
+
+
+                        targets.RemoveAt(0);
+                        if (groupDetails[angle].Count == 0)
+                            groupDetails.Remove(angle);
+                        eliminated++;
+
+                        if (eliminated == 200)
+                            break;
+                    }
+                }
+
+            }
+            //1532 too high 631???  2????? Nope
+            Console.WriteLine($"Q2: {target.X * 100 + target.Y}");
+
+        }
+
+
+        static void Q1()
+        {
+            Vectors(split(Input.real));
         }
 
         static void Q2()
         {
+
+            Eliminate(split(Input.example3), new Point(11, 13));
+            //Best Point X= 17,Y = 22
+            Eliminate(split(Input.real), new Point(17, 22));
         }
     }
 }
---
+++ 2019/Day10/TargetDetails.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Windows;
+
+namespace Day10
+{
+    public class TargetDetails
+    {
+        public int Distance;
+        public Point Location;
+        public Point Vector;
+        public decimal Degrees;
+
+        public static TargetDetails CreateTarget(Point shooter, Point target)
+        {
+            return new TargetDetails(shooter, target);
+        }
+
+
+        public TargetDetails(Point shooter, Point target)
+        {
+            Location = target;
+            DetermineVector(shooter, target);
+            DetermineDistance(shooter, target);
+            DetermineDegrees();
+
+        }
+
+        public void DetermineDegrees()
+        {
+            Degrees = Math.Round((decimal)Math.Atan2(Vector.Y, Vector.X) * (decimal)(180 / Math.PI), 1);
+
+            while (Degrees < 0)
+                Degrees += 360;
+
+            while (Degrees > 360)
+                Degrees -= 360;
+        }
+
+        public void DetermineDistance(Point shooter, Point target)
+        {
+            Distance = Math.Abs(target.X - shooter.X) + Math.Abs(target.Y - shooter.Y);
+        }
+
+        public void DetermineVector(Point shooter, Point target)
+        {
+            var vector = new Point(target.X - shooter.X, target.Y - shooter.Y);
+            if (vector.X == 0 || vector.Y == 0)
+            {
+                vector.X = vector.X == 0 ? 0 : vector.X / Math.Abs(vector.X);
+                vector.Y = vector.Y == 0 ? 0 : vector.Y / Math.Abs(vector.Y);
+            }
+            else
+            {
+                var gcd = GCD(vector.X, vector.Y);
+                vector = new Point(vector.X / gcd, vector.Y / gcd);
+            }
+
+            this.Vector = vector;
+        }
+
+        private static int GCD(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if (b == 0)
+                return 0;
+
+            // Pull out remainders.
+            while (true)
+            {
+                int remainder = a % b;
+                if (remainder == 0) return b;
+                a = b;
+                b = remainder;
+            };
+        }
+
+    }
+}
