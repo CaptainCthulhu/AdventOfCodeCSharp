f02ead4692ddb693d82f0ebd3f3e558b18eeb917
CaptainCthulhu <theonegod@gmail.com>
2019-12-12 19:57:37 -0700

    Initial Items

---
+++ 2019/Day12/Planet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Day12
+{
+    class Planet
+    {
+        public Vector3 location;
+        public Vector3 velocity;
+
+        public Planet(int x, int y, int z)
+        {
+            velocity = new Vector3();
+            location = new Vector3(x, y, z);
+        }
+    }
+}
---
+++ 2019/Day12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Day12
+{
+    class Program
+    {
+
+        static Planet[] planets = new Planet[] {                
+            new Planet(3,6,6),
+            new Planet(10,7,-9),
+            new Planet(-3,7,9),
+            new Planet(-8,0,4)
+        };   
+
+        static void Main(string[] args)
+        {
+            Q1();
+            Q2();
+        }
+
+        static private void Q1()
+        {
+            int iteration = 0;
+            int goal = 1000;
+            while (iteration < goal)
+            {
+
+                foreach (var x in planets)
+                {
+                    //hard stuff in here
+                }
+
+                iteration++;
+            }
+
+        }
+
+        static private void Q2()
+        {
+
+        }
+    }
+}
