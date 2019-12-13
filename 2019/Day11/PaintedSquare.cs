cf19c172bc53e1a98b8d3c7181775462c020efa3
CaptainCthulhu <theonegod@gmail.com>
2019-12-12 19:45:00 -0700

    Day11 (#4)
    * Just started
    
    * Overengineered the hell outta this.
    
    * What do you know? It worked!
    
    * Better presentation.

---
+++ 2019/Day11/PaintedSquare.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Day11
+{
+    public class PaintedSquare
+    {
+        public long color;
+        public Point location;
+
+        public PaintedSquare(long color, Instruction last)
+        {
+            this.color = color;
+            if (last != null)
+                this.location = last.location;
+            else
+                this.location = new Point(0, 0);
+        }
+    }
+}
